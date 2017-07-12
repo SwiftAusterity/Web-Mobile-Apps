4984a61bfa632ed69df5e5554a52e514cf41e2ee
Danny Nissenfeld <SwiftAusterity@users.noreply.github.com>
2017-07-12 08:58:38 -0500

    Add files via upload

--- ChromeExtensions/Twueism/source/twueism.js
+++ ChromeExtensions/Twueism/source/twueism.js
@@ -1,5 +1,5 @@
 chrome.webNavigation.onCompleted.addListener(function(details) {
     chrome.tabs.executeScript(details.tabId, {
-        code: "[].slice.call(document.querySelectorAll('p,span')).forEach(function (e) { e.innerHTML = e.innerHTML.replace(/true/gi, 'twue') })"
+        code: "[].slice.call(document.querySelectorAll('p,span,h1,h2,h3,h4,h5,h6')).forEach(function (e) { e.innerHTML = e.innerHTML.replace(/(true) (dominant|submissive|dom|sub)/gi, \"<span style='color:#FFC300;font-weight:bold'>twue $2</span>\").replace(/true/gi, \"<span style='color:#FFC300;font-weight:bold'>twue</span>\");});"
     });
 });
