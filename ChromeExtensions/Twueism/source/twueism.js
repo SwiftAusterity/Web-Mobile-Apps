5753419b96e6016a7794a1f2186b815d67063ed2
Danny Nissenfeld <SwiftAusterity@users.noreply.github.com>
2017-07-12 12:13:44 -0500

    Add files via upload

--- ChromeExtensions/Twueism/source/twueism.js
+++ ChromeExtensions/Twueism/source/twueism.js
@@ -1,5 +1,5 @@
 chrome.webNavigation.onCompleted.addListener(function(details) {
     chrome.tabs.executeScript(details.tabId, {
-        code: "[].slice.call(document.querySelectorAll('p,span,h1,h2,h3,h4,h5,h6')).forEach(function (e) { e.innerHTML = e.innerHTML.replace(/(true) (dominant|submissive|dom|sub)/gi, \"<span style='color:#FFC300;font-weight:bold'>twue $2</span>\").replace(/true/gi, \"<span style='color:#FFC300;font-weight:bold'>twue</span>\");});"
+        code: "[].slice.call(document.querySelectorAll('p,span,h1,h2,h3,h4,h5,h6')).forEach(function (e) { e.innerHTML = e.innerHTML.replace(/([tT])R([uU][eE]) ([dD][oO][mM][iI][nN][aA][nN][tT]|[sS][uU][bB][mM][iI][sS][sS][iI][vV][eE]|[dD][oO][mM]|[sS][uU][bB])/g, \"<span style='color:#FFC300;font-weight:bold'>$1W$2 $3</span>\").replace(/(t)r(ue) (dominant|submissive|dom|sub)/gi, \"<span style='color:#FFC300;font-weight:bold'>$1w$2 $3</span>\").replace(/([tT])R([uU][eE])/g, \"<span style='color:#FFC300;font-weight:bold'>$1W$2</span>\").replace(/(t)r(ue)/gi, \"<span style='color:#FFC300;font-weight:bold'>$1w$2</span>\");});"
     });
 });
